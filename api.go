33c9777436abe607fff357966f97156307e1b30c
Samuel A. Falvo II <sam.falvo@rackspace.com>
2013-06-21 15:12:14 -0700

    bogus imports

--- api.go
+++ api.go
@@ -1,5 +1,11 @@
 package foo
 
+import (
+  _ "github.com/sam-falvo/test-repo-1"
+  _ "github.com/sam-falvo/test-repo-2"
+  _ "github.com/sam-falvo/test-repo-3"
+)
+
 func Sum(a, b int) int {
 	return a + b
 }
