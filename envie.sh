99220d13f6d3a73e61eaf6b17c7c9c8f7cd0fda7
Samuel A. Falvo II <sam.falvo@rackspace.com>
2013-06-21 10:51:19 -0700

    envie script to wrap Go package into its own project workspace

---
+++ envie.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# This script wraps a bare git checkout into its own Go project workspace.
+# It does this through symbolic link magic.
+# src/.../pkgname -> .
+
+# Configuration
+PKGBASE=github.com/sam-falvo
+PKGNAME=envie
+
+# Get canonical directory name of build script, and change to that directory,
+# so we always know where we are.
+CWD=$(dirname $0)
+CWD=$(cd $CWD; pwd)
+
+# Establish Go project environment.
+export GOPATH=$(pwd)
+mkdir -p src/$PKGBASE
+ln -s $(pwd) src/$PKGBASE/$PKGNAME
+
+# Remind user to set GOPATH.
+echo "Reminder -- environment variables set in this program do not"
+echo "propegate to your interactive shell session.  You'll want to"
+echo "execute the following command if you haven't set the GOPATH"
+echo "variable already:"
+echo ""
+echo "export GOPATH=$(pwd)"
+
